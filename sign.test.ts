b0168f57f230d72a69b5f94f51fe86c26d073ed1
c8e4 <c8e4@proton.me>
2024-06-09 05:37:03 +0200

    refactor

--- sign.test.ts
+++ sign.test.ts
@@ -1,11 +1,12 @@
-import { ALICE_ADDRESS, ALICE_MNEMONIC, POOL_ADDRESS, POOL_MNEMONIC, utxo } from './constants';
+import { ALICE_ADDRESS, ALICE_MNEMONIC, BLOCKCHAIN_PARAMETERS, BOB_MNEMONIC, POOL_ADDRESS, POOL_MNEMONIC, utxo } from './constants';
 import { signTxInput } from './sign';
 import { describe, it, expect, beforeAll } from 'vitest';
-import { AvlTree$, ProverBuilder$, ReducedInputData } from 'sigmastate-js/main';
+import { AvlTree$, ProverBuilder$, ReducedInputData, ReducedTransaction } from 'sigmastate-js/main';
 import bip39 from 'bip39';
 import { headers, jsonHeaders, sigmaJsHeader } from 'fakeContext';
 import { OutputBuilder, SAFE_MIN_BOX_VALUE, TransactionBuilder } from '@fleet-sdk/core';
 import { EIP12UnsignedTransaction, UnsignedTransaction } from '@fleet-sdk/common';
+type SignedTransaction = import("./node_modules/sigmastate-js/node_modules/@fleet-sdk/common/dist/esm/types/transactions").SignedTransaction;
 
 const network = 0;
 const height = 1282261;
@@ -34,47 +35,13 @@ describe('UnsignedTransaction', () => {
 	});
 
 	it('sigmastate.js can reduceTransactionInput', async () => {
-		//https://api.ergoplatform.com/api/v1/info
-		const info = {
-			lastBlockId: '5b9b19ac028c6956b4cdf8ec75227934b8134ff3635ed3aceac8a8bf20788dce',
-			height: 1282261,
-			maxBoxGix: 6197269,
-			maxTxGix: 1167892,
-			params: {
-				height: 506880,
-				storageFeeFactor: 1250000,
-				minValuePerByte: 360,
-				maxBlockSize: 1271009,
-				maxBlockCost: 7030268,
-				blockVersion: 2,
-				tokenAccessCost: 100,
-				inputCost: 2000,
-				dataInputCost: 100,
-				outputCost: 100,
-			},
-		};
-		const BLOCKCHAIN_PARAMETERS = {
-			storageFeeFactor: info.params.storageFeeFactor,
-			minValuePerByte: info.params.minValuePerByte,
-			maxBlockSize: info.params.maxBlockSize,
-			tokenAccessCost: info.params.tokenAccessCost,
-			inputCost: info.params.inputCost,
-			dataInputCost: info.params.dataInputCost,
-			outputCost: info.params.outputCost,
-			maxBlockCost: info.params.maxBlockCost,
-			softForkStartingHeight: 100,
-			softForkVotesCollected: 50,
-			blockVersion: info.params.blockVersion,
-		};
 		const buffer = await bip39.mnemonicToSeed(POOL_MNEMONIC);
 		const mnemonicPhrase = buffer.toString('hex');
 
-		const builder = ProverBuilder$.create(BLOCKCHAIN_PARAMETERS, network).withMnemonic(
+		const prover = ProverBuilder$.create(BLOCKCHAIN_PARAMETERS, network).withMnemonic(
 			mnemonicPhrase,
 			'',
-		);
-
-		const prover = builder.build();
+		).build();
 
 		const xHeaders = jsonHeaders.map(sigmaJsHeader);
 
@@ -84,7 +51,7 @@ describe('UnsignedTransaction', () => {
 			sigmaPreHeader: xHeaders[0],
 		};
 
-		const data: ReducedInputData = prover.reduceTransactionInput(
+		const reducedInput: ReducedInputData = prover.reduceTransactionInput(
 			stateCtx,
 			unsignedTx,
 			unsignedTx.inputs,
@@ -92,7 +59,23 @@ describe('UnsignedTransaction', () => {
 			[],
 			0,
 		);
-		console.log(data)
-		expect(data).toBeDefined();
+		expect(reducedInput).toBeDefined();
+
+		const reducedTx: ReducedTransaction = prover.reduce(
+			stateCtx,
+			unsignedTx,
+			unsignedTx.inputs,
+			unsignedTx.dataInputs,
+			[],
+			0,
+		);
+		expect(reducedTx).toBeDefined();
+
+		const hints = prover.generateCommitments(reducedTx);
+		expect(hints).toBeDefined();
+		//console.dir(hints, {depth:null})
+		
+		//const signedTx: SignedTransaction = prover.signReduced(reducedTx, hints);
+		//expect(signedTx).toBeDefined();
 	});
 });
